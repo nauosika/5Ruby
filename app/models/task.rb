9255559ddfa4e83e6ed10cac15717520c385b028
nauosika <79121183+nauosika@users.noreply.github.com>
2021-11-16 21:31:42 +0800

    步驟12: 資料驗證 (#8)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
-  # scope :updated_desc, -> { order(updated_at: :desc) }
+
   scope :created_desc, -> { order(created_at: :desc) }
+
+  validates :description, presence: true, length: { minimum: 5 }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context '資料限制' do
+      it '不能空白' do
+        task = Task.new(description: "")
+        expect(task).not_to be_valid
+      end
+
+      it '不能低於5個字' do
+        task = Task.new(description: "去睡覺")
+        expect(task).not_to be_valid
+      end
+    end
+  end
 end
