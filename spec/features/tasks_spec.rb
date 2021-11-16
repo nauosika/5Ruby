ae056278315de41556ad58b1125f9495cce4700c
nauosika <nauosikaforwork@gmail.com>
2021-11-16 19:13:58 +0800

    setp11

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
+  context 'task index init load' do
+    scenario 'order by created_at' do
+      task1 = Task.create!(:description => "內容一")
+      task2 = Task.create!(:description => "內容二")
+      visit tasks_path
+
+      first_task = all('table>tbody>tr>td').first
+      expect(first_task).to have_text("內容二")
+    end
+  end
+
   context 'carete new task' do
     scenario 'succesful' do
       visit new_task_path
-        fill_in 'description', with: '測試內容'
-        fill_in 'end_at', with: Time.now
-        click_button 'Create Task'
-        expect(page).to have_content('Task was succesfully created')
+      expect(page).to have_content('新增任務')
+
+      within('form') do
+        fill_in '內容', with: '測試內容'
+        click_button '送出'
+      end
+        expect(page).to have_content('新增成功')
+
+        task = Task.last
+        expect(task.description).to eq('測試內容')
     end
   end
 end
