3012328dbdf14e11e96c086e2287c717472e56f7
nauosika <nauosikaforwork@gmail.com>
2021-11-17 22:29:24 +0800

    可以結束排序

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
 
   scope :created_desc, -> { order(created_at: :desc) }
+  scope :end_at_desc, -> { order(end_at: :desc) }
 
   validates :description, presence: true, length: { minimum: 5 }
   validates :end_at, presence: true
